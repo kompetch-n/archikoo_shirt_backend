bd3a583f95864e88e19a119078f4cb9025ba8608
kompetch-n <70504628+kompetch-n@users.noreply.github.com>
2025-11-03 20:32:15 +0700

    Deploy vercel

--- main.py
+++ main.py
@@ -116,5 +116,5 @@ async def get_all():
     return customers
 
 # ===== รันเซิร์ฟเวอร์ =====
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
